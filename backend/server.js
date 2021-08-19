34047c3f0e961d20affbecf483b6cb8dd1652c63
KavinSuraweera <87318518+KavinSuraweera@users.noreply.github.com>
2021-08-20 00:19:16 +0530

    Backend added

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+//import Routes
+const packageRoutes = require('./routes/packages.js');
+
+//app midleware
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/package", packageRoutes);
+ 
+const PORT = 8000;
+const DB_URL = 'mongodb+srv://user:user@busapp.1dmsk.mongodb.net/bus?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL,{
+    useNewUrlParser :true,
+    useUnifiedTopology : true  
+})
+
+.then(()=>{
+    console.log(`----------------- DB connectd Sucessfully ! -----------------`)
+}).catch((err)=>{
+    console.log(`DB connection error`, err.message)
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App is running on port ${PORT}`)
+});
