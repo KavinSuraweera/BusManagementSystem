055ed2ae4f50587836c298fbe973d52d7a8196fe
KavinSuraweera <87318518+KavinSuraweera@users.noreply.github.com>
2021-08-20 17:33:41 +0530

    booking page debugged

--- frontend/src/components/booking.js
+++ frontend/src/components/booking.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function booking() {
     return (
         <div>
-            <h1>This is my booking page</h1>
+            <h1>This is my booking 123</h1>
         </div>
     )
 }
