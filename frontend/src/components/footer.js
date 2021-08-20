53afbd32f346c92afbb6cda56a232e4ff79ebf36
panchani <panchanimadanayake@gmail.com>
2021-08-20 18:42:08 +0530

    footerchanged-panchi

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -5,7 +5,7 @@ export default function footer() {
         <div>
             <div className="fixed-footer">
                 <p>Fixed footer</p>
-                <p>Contact us:  </p>
+                <p>Contact us: Panch</p>
                 <div></div>
             </div>
         </div>
