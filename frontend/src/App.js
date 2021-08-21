249b939420b67861577bc1fb902dfcf2617f048a
KavinSuraweera <87318518+KavinSuraweera@users.noreply.github.com>
2021-08-21 10:30:12 +0530

    header updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Route path="/" exact component={Home}/>
         <Route path="/bookings" exact component={Booking} />
         
-        <Footer/>
       </Router>
     </div>
   );
