71d7e0177d0ac4c7cd743921e3cf1c58dce14f14
shehan.kaushalye <it20401344@my.sliit.lk>
2021-08-22 21:44:06 +0530

    nikn wedd beluwe

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -5,7 +5,7 @@ export default function footer() {
         <div>
             <div className="fixed-footer">
                 <p>Fixed footer</p>
-                <p>Contact us: </p>
+                <p>Contact us: hello </p>
                 <div></div>
             </div>
         </div>
