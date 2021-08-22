980768d79d5b8bac90615570cdaa208e506c6086
KavinSuraweera <it20191474@my.sliit.lk>
2021-08-22 22:15:35 +0530

    aa

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -4,11 +4,7 @@ export default function footer() {
     return (
         <div>
             <div className="fixed-footer">
-                <p>Fixed footer</p>
-                <p>Contact us: hello </p>
-                <p>Contact us: Panch </p>
-                <p>Contact us: </p>
-                <p>Contact us: Panc , Navee</p>
+                 
                 <div></div>
             </div>
         </div>
