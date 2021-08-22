fbdf5a401fe8deccfd0177579aa248f764697314
KavinSuraweera <it20191474@my.sliit.lk>
2021-08-22 22:03:04 +0530

    asdsf

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -5,19 +5,10 @@ export default function footer() {
         <div>
             <div className="fixed-footer">
                 <p>Fixed footer</p>
-<<<<<<< HEAD
-<<<<<<< HEAD
                 <p>Contact us: hello </p>
-=======
-<<<<<<< HEAD
                 <p>Contact us: Panch </p>
-=======
                 <p>Contact us: </p>
->>>>>>> 6bebe53de8ead63dd336bec96d09b772b685d6ad
->>>>>>> 15b85ec56b13fa9bee4af72e6faf558afc6ffc4e
-=======
                 <p>Contact us: Panch , Navee</p>
->>>>>>> 29e948f540520a4c32900c8b62980c66252ff9ec
                 <div></div>
             </div>
         </div>
