86fb8042cb8ae11c3313b172858c3fdcb0124ccf
YasiruOps <yasirunayanasaraopanayaka@gmail.com>
2021-09-09 12:37:02 +0530

    dash update

--- frontend/src/components/dashbord/sidebar/sidebar.jsx
+++ frontend/src/components/dashbord/sidebar/sidebar.jsx
@@ -7,8 +7,8 @@ import busmanagement from "../../../img/dashbord/busmanagement.jpg";
 import empmanage from "../../../img/dashbord/calendar.png";
 import packagemanage from "../../../img/dashbord/packagemanage.jpg";
 import MenuIcon from '@material-ui/icons/Menu';
-import { Link } from "react-router-dom";
 
+import { Link } from 'react-router-dom'
 
 export default function sidebar() {
     return (
@@ -22,32 +22,40 @@ export default function sidebar() {
                         </h3>
                     </div>
                     <ul className="sidebarList">
+                        <Link className="Link" to="/dashbord">
                         <li className="sidebarListItem active">
                             <img class="image1" src={home}/>
                             Dashbord
                         </li>
+                        </Link>
+                        <Link className="Link" to="./customermain">
                         <li className="sidebarListItem">
                             <img class="image1" src={manageemployee}/>
                             User Management
                         </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <img class="image1" src={ownermanage}/>
                             Owner Management
                         </li>
+                        
                         <li className="sidebarListItem">
                             <img class="image1" src={shedulemanage}/>
                             Shedule Management
                         </li>
-                        <Link to="/bus">
+
+                        <Link className="Link" to="./employeemain">
                         <li className="sidebarListItem">
                             <img class="image1" src={busmanagement}/>
                             Bus Management
                         </li>
                         </Link>
+                        <Link className="Link" to="./employeemain">
                         <li className="sidebarListItem">
                             <img class="image1" src={empmanage}/>
                             Emp Management
                         </li>
+                        </Link>
                         <li className="sidebarListItem">
                             <img class="image1" src={packagemanage}/>
                             Package Management
