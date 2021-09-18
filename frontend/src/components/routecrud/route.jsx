e4531e9a682192451e27c28255204ad10c74a155
panchani <panchanimadanayake@gmail.com>
2021-09-19 03:18:16 +0530

    Route Frontend Updated

--- frontend/src/components/routecrud/route.jsx
+++ frontend/src/components/routecrud/route.jsx
@@ -1,9 +1,198 @@
-import React from 'react'
+import React, { useState, useEffect, useForms } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
+import '../../CSS/App.css';
 
-export default function route() {
+
+export default function Addpackage(props) {
+
+    const { recordForEdit } = props;
+
+    const [route, setRoute] = useState({});
+
+    const [routeId, setrouteId] = useState("");
+    const [routeName, setrouteName] = useState("");
+    const [to, setto] = useState("");
+    const [from, setfrom] = useState("");
+    const [pAdult, setaAdult] = useState("");
+    const [pChild, setpChild] = useState("");
+    const [pStudent, setStudent] = useState("");
+ 
+
+
+    function sendData() {
+
+
+
+        const newBus = {
+            id: '0',
+            routeId,
+            routeName,
+            to,
+            from,
+            pAdult,
+            pChildt,
+            pStudent,
+        }
+
+
+        axios.post("http://localhost:8000/route/add", newRoute).then(() => {
+
+            window.location.reload(false);
+
+        }).catch((err) => {
+            alert(err)
+        })
+
+    }
+
+    console.log(route)
+
+    const updateRoute = {
+        routeId,
+        routeName,
+        to,
+        from,
+        pAdult,
+        pChild,
+        pStudent
+    };
+
+    function editBus(uId) {
+
+        axios
+            .put(
+                `http://localhost:8000/route/update/${uId}`,
+                updateRoute
+            )
+            .then((res) => {
+                alert("Route Updated");
+                window.location.reload(false);
+                //this.setState({ redirect: "/home" });
+            })
+            .catch((err) => {
+                alert(err);
+            });
+    }
+
+
+
+
+
+    useEffect(() => {
+        if (recordForEdit != null) {
+            setRoute({
+                ...recordForEdit
+            })
+
+            setrouteId(recordForEdit.routeId)
+            setrouteName(recordForEdit.routeName)
+            setto(recordForEdit.to)
+            setfrom(recordForEdit.from)
+            setpAdult(recordForEdit.pAdult)
+            setpChild(recordForEdit.pChild)
+            setpStudent(recordForEdit.pStudent)
+         
+        }
+    }, [recordForEdit]);
+
+
+
+
+    const handleSubmit = (e) => {
+        if (route._id == null)
+
+            sendData(route)
+        else {
+
+
+            editBus(route._id)
+        }
+
+    }
+
+
+
+
     return (
-        <div>
-            
+        <div className="container">
+            <form className="row g-3" onSubmit={(e) => { handleSubmit(e) }}>
+                <div className="col-md-6">
+                    <label htmlFor="packageName" className="form-label">Enter Route ID:</label>
+                    <input type="text" className="form-control" id="packageName" placeholder="Enter Route Number"
+                        value={routeId}
+                        onChange={(e) => {
+                            setrouteId(e.target.value);
+                        }}
+                    />
+                </div>
+
+                <div className="input-group">
+                    <label htmlFor="packageName" className="form-label">Enter Route Name:</label>
+                    <input type="text" className="form-control" id="packageName" placeholder="Enter Route Name"
+                        value={routeName}
+                        onChange={(e) => {
+                            setrouteName(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className="col-md-2">
+                    <label htmlFor="tripsCount" className="form-label">Enter To</label>
+                    <input type="text" className="form-control" id="tripsCount" placeholder="Enter To"
+                        value={to}
+                        onChange={(e) => {
+                            setto(e.target.value);
+                        }}
+                    />
+                </div>
+                <div className="col-md-2">
+                    <label htmlFor="tripsCount" className="form-label">Enter From</label>
+                    <input type="tel" className="form-control" id="phone" placeholder="Enter From"
+                        value={from}
+                        onChange={(e) => {
+                            setfrom(e.target.value);
+                        }}
+                    />
+                </div>
+
+                <div className="col-md-6">
+                    <label htmlFor="timePeriod" className="form-label">Enter Adult Name:</label>
+                    <input type="text" className="form-control" id="timePeriod" placeholder="Enter Adult Name"
+                        value={pAdult}
+                        onChange={(e) => {
+                            setpAdult(e.target.value);
+                        }}
+                    />
+                </div>
+
+                <div className="col-md-6">
+                    <label htmlFor="timePeriod" className="form-label">Enter Child Name:</label>
+                    <input type="text" className="form-control" id="timePeriod" placeholder="Enter Child Name"
+                        value={pChild}
+                        onChange={(e) => {
+                            setpChild(e.target.value);
+                        }}
+                    />
+                </div>
+
+                <div className="col-md-6">
+                    <label htmlFor="timePeriod" className="form-label">Enter Student Name:</label>
+                    <input type="text" className="form-control" id="timePeriod" placeholder="Enter Student Name"
+                        value={pStudent}
+                        onChange={(e) => {
+                            setpStudent(e.target.value);
+                        }}
+                    />
+                </div>
+
+              
+
+                <div>
+                    <input type="submit" className="btn btn-primary" href="/home" value="Submit" />
+                </div>
+            </form>
+
         </div>
     )
 }
+
--- frontend/src/components/routecrud/routeform.jsx
+++ frontend/src/components/routecrud/routeform.jsx
@@ -1,9 +1,192 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Popup from "../../components/popup";
+import Addroute from './route';
+import Header from '../header'
 
-export default function fouteform() {
+export default function Allroute() {
+
+    const [recordForEdit, setRecordForEdit] = useState(null);
+    const [openPopup, setOpenPopup] = useState(false);
+    const [route, setRoute] = useState([]);
+
+
+
+
+
+    function sendData(e) {
+
+        e.preventDefault();
+
+
+
+        axios.post("http://localhost:8000/route/add", route).then(() => {
+            // alert("Route added!")
+            window.location.reload(false);
+
+        }).catch((err) => {
+            alert(err)
+        })
+
+    }
+
+    useEffect(() => {
+        const getRoute = () => {
+            axios.get("http://localhost:8000/route/").then((res) => {
+                setBus(res.data);
+            }).catch((err) => {
+                alert(err.message)
+            })
+        }
+        getRoute();
+    }, [])
+
+
+
+    // const [pId, setId] = useState("")
+    // function sendId(pId) {
+
+
+
+
+    // }
+
+
+    function onDelete(rId) {
+        axios.delete(`http://localhost:8000/route/delete/${rId}`).then((req, res) => {
+            window.location.reload(false);
+        }).catch((err) => {
+            alert(err);
+        })
+    }
+
+
+
+
+    const openInPopup = route => {
+        setRecordForEdit(route);
+        setOpenPopup(true);
+        console.log(route)
+    }
+
+
+
+
+
+
+
+    const addOrEdit = (route) => {
+
+        const rid = route._id
+
+
+        axios.put(`http://localhost:8000/route/update/${rid}`, route).then(() => {
+            alert("Updated")
+            window.location.reload(false);
+        }).catch((err) => {
+            alert(err)
+        })
+
+    }
+
+
+
+
+    // update error fixed ---------------------
+
+
+    const [updateBtn, setUpdatebtn] = useState(false);
+    console.log(updateBtn);
+
+    // const updateBtnactive = () =>{
+
+    //      updateBtn? setUpdatebtn(true):setUpdatebtn(false);
+    //     }
+
+
+
+
+    //------------------------------------------ 
     return (
         <div>
-            
+            <Header />
+            <div className="container">
+
+
+
+
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Route ID</th>
+                            <th scope="col">Route Name</th>
+                            <th scope="col">To</th>
+                            <th scope="col">From</th>
+                            <th scope="col">Adult</th>
+                            <th scope="col">Child</th>
+                            <th scope="col">Student</th>
+                        
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {route.map((route, index) => (
+                            <tr key={index}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{route.routeId}</td>
+                                <td>{route.routeName}</td>
+                                <td>{route.to}</td>
+                                <td>{route.from}</td>
+                                <td>{route.pAdult}</td>
+                                <td>{route.pChild}</td>
+                                <td>{route.pStudent}</td>
+
+                                <td>
+                                    <button type="button" class="btn btn-primary">
+                                        <i class="far fa-eye"></i>&nbsp;View
+                                    </button>
+                                    &nbsp;
+                                    <button
+                                        className="btn btn-warning"
+                                        onClick={() => {
+                                            openInPopup(bus);
+                                            setUpdatebtn(true);
+                                        }}
+
+
+                                    >
+                                        <i className="fas fa-edit"></i>&nbsp;Update
+                                    </button>
+                                    &nbsp;
+                                    <button className="btn btn-danger" href="/add" onClick={() => { onDelete(route._id) }} >
+                                        <i className="far fa-trash-alt"></i>&nbsp;Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <button className="btn btn-success"
+                    onClick={() => {
+                        setOpenPopup(true);
+                        setUpdatebtn(false);
+                    }}>
+                    Add new Route</button>
+                <Popup
+                    title={updateBtn ? "Update Route form" : "Add new Route form"}
+                    openPopup={openPopup}
+                    setOpenPopup={setOpenPopup}
+                >
+                    <Addbus
+                        recordForEdit={recordForEdit}
+                        addOrEdit={addOrEdit}
+                    />
+
+                </Popup>
+
+
+            </div>
         </div>
     )
 }
