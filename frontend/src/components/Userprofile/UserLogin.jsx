b35a2fe8290f4132933aa5fc817800c59f5985ed
KavinSuraweera <87318518+KavinSuraweera@users.noreply.github.com>
2021-09-21 05:48:22 +0530

    aa

--- frontend/src/components/Userprofile/UserLogin.jsx
+++ frontend/src/components/Userprofile/UserLogin.jsx
@@ -1,22 +1,22 @@
-import axios from 'axios'
-import { useHistory } from 'react-router-dom';
-import React, { useState } from 'react';
-import './ProfileCSS/Userlogin.css'
-import { useDispatch, useSelector } from 'react-redux';
-import {setid} from  '../../actions/authAction';
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import "./ProfileCSS/Userlogin.css";
+import { useDispatch, useSelector } from "react-redux";
+import { setid } from "../../actions/authAction";
+import Header from "../header";
 
 export default function UserLogin() {
-
   const dispatch = useDispatch();
 
-    const history = useHistory();
+  const history = useHistory();
 
   const initialValues = {
     email: "",
     password: "",
   };
 
-  const[loginForm, setLoginForm] = useState(initialValues);
+  const [loginForm, setLoginForm] = useState(initialValues);
 
   const onChange = (e) => {
     setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
@@ -27,7 +27,7 @@ export default function UserLogin() {
       Email: loginForm.email,
       Password: loginForm.password,
     };
-    
+
     console.log(payload);
     axios
       .post(`http://localhost:8000/customer/login`, payload)
@@ -35,23 +35,59 @@ export default function UserLogin() {
         dispatch(setid(response.data._id));
         console.log(response);
         history.push(`/Userprofile`);
-        
       })
       .catch((err) => {
-       alert(err?.response?.data?.msg);
+        alert(err?.response?.data?.msg);
       });
   };
 
   return (
-    <div className="login-container">
-      <label for="email">Enter Email</label><br/>
-      <input type="text" name="email" onChange={onChange}/>
-      <br />
-      <label for="password">Enter Password</label><br/>
-      <input type="text" name="password" onChange={onChange}/>
-      <br />
-      <br />
-      <button onClick={submit}>Submit</button>
+    <div>
+      <div className="usr_backgound">
+        <Header/>
+        <div className="inner-container">
+          <div className="form">
+            <div className="header">
+              <h2>WELCOME BACK</h2>
+              <hr className="linebar"/>
+            </div>
+            <br/>
+            <br/>
+            <label className="classheader" for="email">
+              Enter Email:
+            </label>
+            <br />
+            <input
+              className="classanswer"
+              type="text"
+              name="email"
+              placeholder="jhonedoe@gmial.com"
+              onChange={onChange}
+            />
+            <br />
+            <br />
+            <label className="classheader" for="password">
+              Enter Password:
+            </label>
+            <br />
+            <input
+              className="classanswer"
+              type="text"
+              name="password"
+              placeholder="X X X X X X"
+              onChange={onChange}
+            />
+            
+            <br />
+            <p className="Forgotp"><a href="#">Forget Password?</a></p>
+            <center>
+            <button className="submitbtn" onClick={submit}>
+              L O G I N
+            </button>
+            </center>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
